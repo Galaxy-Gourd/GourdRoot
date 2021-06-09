4606ae31c5372936c0eb0f40697d87bd6893653d
Michael Stephens <mstephens.dev@gmail.com>
2021-06-09 09:20:45 -0400

    module changes

--- Runtime/Module/IModule.cs
+++ Runtime/Module/IModule.cs
@@ -2,9 +2,6 @@ namespace GG.Data.Base
 {
     public interface IModule
     {
-        /// <summary>
-        /// Called on the module when client has been initialialized
-        /// </summary>
-        void OnModuleClientReady();
+        
     }
 }
--- Runtime/Module/Module.cs
+++ Runtime/Module/Module.cs
@@ -1,43 +1,48 @@
+using System;
+
 namespace GG.Data.Base
 {
     /// <summary>
     /// Base class for core module; ensures proper loading after creation.
     /// </summary>
-    /// <typeparam name="T">DataConfigModule type</typeparam>
-    public abstract class Module<T> : IModule
-        where T : DataConfigModule
+    public abstract class Module<T1, T2>
+        where T1 : DataConfigModule
+        where T2 : DataModuleInitialization
     {
         #region VARIABLES
-        
-        /// <summary>
-        /// The configuration data for this module.
-        /// </summary>
-        protected readonly T _moduleData;
+
+        private readonly Action<T2>[] _callbacks;
 
         #endregion VARIABLES
-
-
+        
+        
         #region CONSTRUCTOR
 
         /// <summary>
         /// Constructs the module with the module data.
         /// </summary>
-        /// <param name="data">The module configuration data.</param>
-        protected Module(T data)
+        protected Module(T1 data, Action<T2>[] callbacks = null)
         {
-            _moduleData = data;
+            _callbacks = callbacks;
+        }
+
+        /// <summary>
+        /// After the module has finished initializing, we may optionally inform listeners with some data
+        /// </summary>
+        /// <param name="initData"></param>
+        protected void DispatchModuleInitializationCallbacks(T2 initData)
+        {
+            // We need to tell Unity-side that we're finished over here
+            if (_callbacks != null)
+            {
+                // Send callbacks
+                foreach (Action<T2> callback in _callbacks)
+                {
+                    callback.Invoke(initData);
+                }
+            }
         }
 
         #endregion CONSTRUCTOR
-
-
-        #region CALLBACKS
-
-        public virtual void OnModuleClientReady()
-        {
-            
-        }
-
-        #endregion CALLBACKS
     }
 }
